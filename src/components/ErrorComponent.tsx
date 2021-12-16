a61a224f948a0e1648fb3e62a0cabfc2edfbc3c9
Samantha Vale <karlasamantha@gmail.com>
2021-12-16 12:16:35 -0800

    adds error component

--- src/components/ErrorComponent.tsx
+++ src/components/ErrorComponent.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
-const ErrorComponent = () => {
+const ErrorComponent = ({ error }: { error: string }) => {
   return (
-    <div></div>
+    <div>
+      <h3>Oops</h3>
+      <p>We apologize, it's not you, it's us :(</p>
+      <p>{error}</p>
+    </div>
   )
 }
 
